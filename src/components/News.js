e818fe9b392d02ffde48f43d0e95428b6aa21a98
Harender singh chauhan <harendersingh7896@gmail.com>
2022-11-09 12:15:19 +0530

    done all

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import PropTypes from 'prop-types'
+
 export class News extends Component {
 
   static defaultProps = {
